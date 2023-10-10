38c20119e5a9d7c53bfc645bb01c5e21cddb8cb2
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-10-10 11:49:25 +0800

    update macros

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -26,36 +24,50 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+fn send_tx(q: Arc<Queue>, tx: Arc<mpsc::Sender<u32>>) -> Result<(), mpsc::SendError<u32>> {
+    // let qc = Arc::new(q);
+    // let qc1 = Arc::clone(&qc);
+    // let qc2 = Arc::clone(&qc);
 
-    thread::spawn(move || {
-        for val in &qc1.first_half {
+    let q1 = Arc::clone(&q);
+    let q2 = Arc::clone(&q);
+    let tx1 = Arc::clone(&tx);
+    let tx2 = Arc::clone(&tx);
+
+    let t1 = thread::spawn(move || {
+        for val in &q1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
-    thread::spawn(move || {
-        for val in &qc2.second_half {
+    let t2 = thread::spawn(move || {
+        for val in &q2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+    Ok(())
 }
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    let queue = Queue::new();
+    // let queue = Queue::new();
+    let queue = Arc::new(Queue::new());
     let queue_length = queue.length;
 
-    send_tx(queue, tx);
+    let tx = Arc::new(tx); // Convert tx to Arc<T> for thread sharing
+    if let Err(err) = send_tx(Arc::clone(&queue), Arc::clone(&tx)) {
+        eprintln!("Error sending data: {:?}", err);
+    }
 
     let mut total_received: u32 = 0;
+    drop(tx);
     for received in rx {
         println!("Got: {}", received);
         total_received += 1;
