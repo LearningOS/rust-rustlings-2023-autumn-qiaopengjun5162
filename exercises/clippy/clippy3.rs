d8f08a7d00b2a471bf4f7f0b751e9109a46dec87
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-10-10 18:30:24 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -5,17 +5,18 @@
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
 #[allow(unused_variables, unused_assignments)]
+#[allow(clippy::let_unit_value)]
 fn main() {
     let my_option: Option<()> = Default::default();
     if my_option.is_none() {
-        panic!("my_option is None");
+        eprintln!("my_option is None");
     }
 
     let my_arr = &[-1, -2, -3, -4, -5, -6];
     println!("My array! Here it is: {:?}", my_arr);
 
-    vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", vec![1, 2, 3, 4, 5]);
+    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
